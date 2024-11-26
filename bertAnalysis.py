0dbd2609482f643daac1007fddfcc6e279489ce6
walace owili <106796931+owiliwalace@users.noreply.github.com>
2024-11-26 10:36:30 +0300

    Update bertAnalysis.py November 26th

--- bertAnalysis.py
+++ bertAnalysis.py
@@ -11,7 +11,7 @@ from torch.nn import CrossEntropyLoss
 csv_file_path = 'C:/Users/User/Desktop/Tweets.csv'
 df = pd.read_csv(csv_file_path)
 
-#october 24th
+#november 26th
 
 for column in ['textID', 'text', 'selected_text', 'sentiment']:
     print(f"Length of {column}: {len(df[column])}")
